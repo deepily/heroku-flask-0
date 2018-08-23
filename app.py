f6222d3b1921e4e7d9171100c725061b7d3e87dd
Rick Ruiz <ricardo.felipe.ruiz@gmail.com>
2018-08-23 15:18:05 -0400

    added gatos

--- app.py
+++ app.py
@@ -1,19 +1,54 @@
 from flask import Flask
 from datetime import datetime
 import os
+import requests
 
 app = Flask(__name__)
 
-@app.route('/')
-def homepage():
+@app.route( "/" )
+def home():
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
 
     return """
-    <h1>Hello heroku</h1>
+        <h1>Like Cats 'n' Dogs!</h1>
+        <p>It is currently {time} GMT.</p>
+        <p>¿Quieres <a href="/doggies">perros</a> o <a href="/kitties">gatos</a>?</p>
+        """.format( time=the_time )
+
+@app.route( '/kitties' )
+def kitties():
+
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+    <h1>Gatos?</h1>
     <p>It is currently {time} GMT.</p>
 
     <img src="http://loremflickr.com/600/400" />
-    """.format(time=the_time)
+    """.format( time=the_time )
+
+@app.route( '/doggies' )
+def get_doggies():
+
+    # From: https://dog.ceo/dog-api/
+    doggie_url = "https://dog.ceo/api/breeds/image/random"
+
+    # sending get request and saving the response as response object
+    response = requests.get( url=doggie_url )
+
+    # extracting data in json format
+    data = response.json()
+    print( data )
+    print( data[ "message" ] )
+
+    the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
+
+    return """
+        <h1>Doggies!</h1>
+        <p>It is currently {time} GMT...</p>
+
+        <img src="{doggie_url}" />
+        """.format( time=the_time, doggie_url=data[ "message" ] )
 
 if __name__ == '__main__':
 
--- foo.py
+++ foo.py
@@ -1,2 +1,18 @@
 import flask
 print( flask.__version__ )
+
+import requests
+#import json
+
+doggie_url = "https://dog.ceo/api/breeds/image/random"
+
+# defining a params dict for the parameters to be sent to the API
+#PARAMS = { "foo":"bar" }
+
+# sending get request and saving the response as response object
+response = requests.get( url=doggie_url )#, params=PARAMS )
+
+# extracting data in json format
+data = response.json()
+print( data )
+print( data[ "message" ] )
