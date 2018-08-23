3596367dc4a8bb6142cc989f90a335feb5c22237
Rick Ruiz <ricardo.felipe.ruiz@gmail.com>
2018-08-23 12:47:38 -0400

    added host spec

--- app.py
+++ app.py
@@ -20,6 +20,6 @@ if __name__ == '__main__':
     # From: https://stackoverflow.com/questions/4906977/how-do-i-access-environment-variables-from-python
     env_port = int( os.getenv( key='PORT', default=8888 ) )
     print( env_port )
-    app.run( port=env_port )
+    app.run( host='0.0.0.0', port=env_port )
 
 
